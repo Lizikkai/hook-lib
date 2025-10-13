e930d89f7d4d682be9f93dbf74cb266536a9923c
lizikai <lzk9863@126.com>
2025-10-13 10:04:41 +0800

    添加设备hook和运行环境hook

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import typescript from "@rollup/plugin-typescript";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  build: {
+    target: "es2015",
+    lib: {
+      entry: resolve(__dirname, "src/index.ts"),
+      name: "HookLib",
+      fileName: (format) => `hook-lib.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        dir: resolve(__dirname, 'dist'),
+        globals: {
+          vue: "Vue"
+        }
+      },
+      plugins: [typescript()]
+    }
+  },
+});
